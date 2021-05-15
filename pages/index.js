4f74b8262c34122768a3f6e18b7c1f3718639498
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-05-16 06:36:57 +0800

    accidentts happen

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home({ posts }) {
                       </div>
                       <div className="text-base font-medium leading-6">
                         <Link
-                          href={`/blog/${slug}`}
+                          href={`/${slug}`}
                           className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
                           aria-label={`Read "${title}"`}
                         >
